71cbd74a6103944cab4bb33651fd1a8f37dd2769
Bruno Andreo <brunolino2026@gmail.com>
2021-02-23 21:47:33 -0300

    added first test

---
+++ tests/unit/application.test.ts
@@ -0,0 +1,17 @@
+import Application from "../../src/server/application";
+
+describe('#Application', () => {
+  let app: Application;
+
+  beforeEach(() => {
+    app = new Application();
+  });
+
+  test('should call listen server status when start method is called', () => {
+    const listen = jest.spyOn(app.app, "listen");
+
+    app.start();
+
+    expect(listen).toHaveBeenCalled();
+  });
+});
--- tests/unit/init.ts
+++
@@ -1,5 +0,0 @@
-describe('Teste', () => {
-  test('should be 4 when 2 + 2', () => {
-    expect(2+2).toBe(4);
-  });
-});
