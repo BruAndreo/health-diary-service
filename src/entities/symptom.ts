43ebb1664bdd0bd5f070ce42f827c9b1517404ee
Bruno Andreo <brunolino2026@gmail.com>
2021-03-02 21:42:41 -0300

    created symptom entity class

---
+++ src/entities/symptom.ts
@@ -0,0 +1,8 @@
+export default class Symptom {
+  private type: string;
+  private description: string;
+  private medicines: string[];
+  private continues: boolean;
+  private startedAt: Date;
+  private finished: Date;
+}
